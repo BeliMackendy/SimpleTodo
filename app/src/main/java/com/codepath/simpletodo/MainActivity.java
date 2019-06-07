395a0fe03d5740b9ea3663cb31bc5c25738e648a
BeliMackendy <belizaire.mackendy@gmail.com>
2019-06-06 23:21:29 -0600

    Add EditItem

--- app/src/main/java/com/codepath/simpletodo/MainActivity.java
+++ app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.codepath.simpletodo;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +20,10 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static int EDIT_REQUEST_CODE=20;
+    public final static String ITEM_TEXT = "itemText";
+    public final static String ITEM_POSITION = "itemPosition";
 
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
@@ -61,6 +67,30 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(MainActivity.this,EditItemActivity.class);
+                i.putExtra(ITEM_TEXT,items.get(position));
+                i.putExtra(ITEM_POSITION, position);
+                startActivityForResult(i,EDIT_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == EDIT_REQUEST_CODE)
+        {
+            String updatedItem = data.getExtras().getString(ITEM_TEXT);
+            int position = data.getExtras().getInt(ITEM_POSITION);
+            items.set(position,updatedItem);
+            itemsAdapter.notifyDataSetChanged();
+            writeItems();
+            Toast.makeText(this,"Item Updated Successfully",Toast.LENGTH_SHORT).show();
+        }
     }
 
     private File getDataFile()
@@ -83,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             FileUtils.writeLines(getDataFile(),items);
         } catch (IOException e) {
-            Log.e("MainActivity","Error reading file",e);
+            Log.e("MainActivity","Error writing file",e);
         }
     }
 }
